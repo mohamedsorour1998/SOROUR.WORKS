0136a91316408502e9a5d27fd2e698de446fb8a3
mohamedsorour1998 <mohamedsorour1998@gmail.com>
2024-04-14 15:55:15 +0200

    Added the certs

--- src/app/pages/certs/certs.component.ts
+++ src/app/pages/certs/certs.component.ts
@@ -9,23 +9,23 @@ export class CertsComponent {
   certifications = [
     {
       title: 'RHCSA: Red Hat Certified System Administrator',
-      logo: 'assets/logos/rhcsa.png',
-      link: 'https://www.redhat.com/en/services/certification/rhcsa',
+      logo: 'assets/logos/red-hat-certified-system-administrator-rhcsa.png',
+      link: 'https://www.credly.com/badges/5d623bf8-92c4-46dd-bd44-e0b823e0ccc7/public_url',
     },
     {
       title: 'GCP ACE: GCP Associate Cloud Engineer',
-      logo: 'assets/logos/gcp.png',
-      link: 'https://cloud.google.com/certification/cloud-engineer',
+      logo: 'assets/logos/associate-cloud-engineer-certification.png',
+      link: 'https://www.credly.com/badges/ed0d658c-d6c4-4ddb-875e-7921ecfcfa3d/public_url',
     },
     {
       title: 'CKA: Certified Kubernetes Administrator',
-      logo: 'assets/logos/cka.png',
-      link: 'https://www.cncf.io/certification/cka/',
+      logo: 'assets/logos/cka-certified-kubernetes-administrator.png',
+      link: 'https://www.credly.com/badges/4286d3d5-4a4a-426f-9db4-436d875d511b/public_url',
     },
     {
       title: 'AWS CCP: AWS Certified Cloud Practitioner',
-      logo: 'assets/logos/aws.png',
-      link: 'https://aws.amazon.com/certification/certified-cloud-practitioner/',
+      logo: 'assets/logos/aws-certified-cloud-practitioner.png',
+      link: 'https://www.credly.com/badges/933fa480-bc9e-461f-b4e3-c748a541aa00/public_url',
     },
   ];
 }
